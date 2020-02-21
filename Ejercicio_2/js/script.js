19553968df13659952104474771aa9054a97bfe1
JuanDuran85 <duran85juan@gmail.com>
2020-02-20 23:12:02 -0300

    finalizando el ejercicio 3

--- Ejercicio_2/js/script.js
+++ Ejercicio_2/js/script.js
@@ -1,4 +1,6 @@
 $(function() {
+	//selecciones mediante atributos en los selectores
 	$('input[required]').addClass('fondo_1');
 	$('input[placeholder="Segundo Nombre"]').addClass('fondo_2');
+	$("input[placeholder*='Correo']").addClass('fondo_3');
 });
