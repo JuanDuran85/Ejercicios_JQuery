f97eb5417838e77a4a6527d1ed9c312a0bf11dac
JuanDuran85 <duran85juan@gmail.com>
2020-02-21 00:04:13 -0300

    agregando metodos para recorrer el DOM

--- Ejercicio_3/js/script.js
+++ Ejercicio_3/js/script.js
@@ -6,4 +6,32 @@ $(function() {
 	// el metodo de JQuery para realizar el proceso anterior es el find
 	var resultado = $("#seccion").find('.personas');
 	console.log(resultado);
+
+	//buscando hijos directos - DOM traversing
+	var resultado2 = $("#seccion").children();
+	console.log(resultado2);
+
+	//el metodo anterior se puede hacer con selectores de css
+	var resultado3 = $("#seccion > *");
+	console.log(resultado3);
+
+	//tambien sepuede especificar un selector en particular
+	var resultado4 = $("#seccion").children("section");
+	console.log(resultado4);
+
+	var resultado4 = $("#seccion > section");
+	console.log(resultado4);
+
+	//utilizando metodos de seleccion especifica
+	//first() last()
+	var resultado5 = $("#seccion").children().first().children('.personas').last();
+	console.log(resultado5);
+
+	//utilizando el metodo prev()
+	var resultado6 = $("#seccion").children().first().children().last().prev().prev();
+	console.log(resultado6);
+
+	//utilizando el metodo next()
+	var resultado7 = $("#seccion").children().first().children().first().next();
+	console.log(resultado7);
 });
