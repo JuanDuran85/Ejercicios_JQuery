a2826173b2a6d9cb502a50747f0ec7854a19cd19
JuanDuran85 <duran85juan@gmail.com>
2020-02-22 21:20:38 -0300

    finalizando el ejercicio 8

--- Ejercicio_8/js/script.js
+++ Ejercicio_8/js/script.js
@@ -1,5 +1,13 @@
 $(function () {
-    $("main__contenedor").on("click", "button", function () {
-        
+    $("#main__contenedor").on("click", "button", function () {
+        var seleccion = $("#main__contenedor select option:selected");
+        var valor = seleccion.val();
+        var precio = seleccion.attr("data-precio");
+        //cualquiera de las dos maneras es valida
+        //var precio = seleccion.data("precio");
+
+        if (precio) {
+            $("#div__resultado").html("<p>Destino seleccionado: "+valor+". El costo es de: "+precio+"USD</p>");
+        }    
     });
 });
