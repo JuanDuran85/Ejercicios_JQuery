8d82e33d2cfdc639d18ea9f40bdfe4fb6bb33d9b
JuanDuran85 <duran85juan@gmail.com>
2020-02-22 21:35:43 -0300

    modificando el ejercicio 8

--- Ejercicio_8/js/script.js
+++ Ejercicio_8/js/script.js
@@ -3,11 +3,23 @@ $(function () {
         var seleccion = $("#main__contenedor select option:selected");
         var valor = seleccion.val();
         var precio = seleccion.attr("data-precio");
-        //cualquiera de las dos maneras es valida
-        //var precio = seleccion.data("precio");
 
         if (precio) {
             $("#div__resultado").html("<p>Destino seleccionado: "+valor+". El costo es de: "+precio+"USD</p>");
-        }    
+        };
+        
     });
+
+    //usando delegacion de eventos con change
+    $("#main__contenedor").on("change", "#select__lugar", function () {
+        console.log("hay cambio");
+        var seleccion2 = $("#select__lugar option:selected");
+        var valor2 = seleccion2.val();
+        var precio2 = seleccion2.data("precio");
+
+        if (precio2) {
+        var valor2 = seleccion2.val();
+            alert(`Su selección fue: ${valor2} y el costo es de: ${precio2}USD`);
+        }
+    })
 });
