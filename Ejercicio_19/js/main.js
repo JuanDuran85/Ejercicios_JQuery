55c14f42b1d97c25a899da3ba10733b873c4011d
JuanDuran85 <duran85juan@gmail.com>
2020-02-23 19:52:34 -0300

    terminando ejercicio 19 usando jsrender como templetemple de HTML-JS

--- Ejercicio_19/js/main.js
+++ Ejercicio_19/js/main.js
@@ -4,27 +4,12 @@ $(function () {
         contentType: 'application/json',
         cache: false //para no utilizar el cache del navegador y se conecte al servidor obligatoriamente.
     }).done(function (respuesta) {
-        console.log(respuesta);
-        var mostrar;
-        $.each(respuesta, function (indexInArray, valueOfElement) { 
-            console.log("Index: "+indexInArray);
-            console.log(valueOfElement);
-            mostrar = '<section id="contenedor__caja" class="contenedor__caja" data-id="'+valueOfElement.id+'">';
-            mostrar += '<img src="'+valueOfElement.imagen+'" >';
-            mostrar += '<div class="titulo__caja">'+valueOfElement.nombre+'</div>';
-            mostrar += '<p>'+valueOfElement.descripcion+'</p>';
-            mostrar += '<div class="precio__caja">'+valueOfElement.precio+'CLP</div>';
-            mostrar += '<button>Agregar a Carro</button>';
-            mostrar += `
-                <div>
-                    <a href="#" class="informa__caja">Más Información Aquí</a>
-                </div>
-                <div class="mas_informacion__caja">
-                    <p>${valueOfElement.masInfo}</p>
-                </div>
-            `;
-
-            $("body").append(mostrar);
+        $.each(respuesta, function () {             
+            var template = $.templates("#theTmpl");
+            console.log(template);
+            var htmlOutput = template.render(respuesta);
+            console.log(htmlOutput);
+            $("#result").html(htmlOutput);
         });
     });
 
