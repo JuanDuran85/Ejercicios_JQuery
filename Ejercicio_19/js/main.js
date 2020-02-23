14052bb3e45fa1687a36e7e5bfb138a4c98091dd
JuanDuran85 <duran85juan@gmail.com>
2020-02-23 20:38:22 -0300

    mejorando ejercicio 19

--- Ejercicio_19/js/main.js
+++ Ejercicio_19/js/main.js
@@ -17,4 +17,17 @@ $(function () {
         event.preventDefault();
         $(this).closest("#contenedor__caja").find(".mas_informacion__caja").toggle(2000);
     });
+
+    var costo = 0.0;
+
+    $("body").on("click","button.compra",function (event) {  
+        event.preventDefault();
+        var identificacion = $(this).closest("#contenedor__caja").data("id");
+        console.log(identificacion);
+        var precio = $(this).closest("#contenedor__caja").find(".precio__caja").text();
+        console.log(parseFloat(precio));
+        costo = costo + parseFloat(precio);
+        console.log(costo);
+        $("#total__costo").text("Total: "+new Intl.NumberFormat("es-CL").format(costo)+" CLP");
+    });
 });
