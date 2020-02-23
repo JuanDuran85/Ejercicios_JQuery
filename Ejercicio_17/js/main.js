8ca0f4a5b65ae3defdee190d0a5bfdd5e7dbb587
JuanDuran85 <duran85juan@gmail.com>
2020-02-23 17:25:35 -0300

    trabajando con AJAX en el ejercicio 17

--- Ejercicio_17/js/main.js
+++ Ejercicio_17/js/main.js
@@ -2,11 +2,18 @@ $(function () {
     $("#caja").on("click","button.caja_boton",function () {  
         //otra manera de hacer los llamados mediante ajax y jquery.
         $.ajax("texto.txt", {
+            //el parametro beforeSend, es un objeto que se ejecuta antes de hacer la llamada mediante una función. 
             beforeSend: function () {
                 $("#resultado").text("Cargando...");
             }
-        }).done(function (response) {  
-            $("#resultado2").html(response);
+            //el metodo done se ejecuta cuando el llamdo de ajax al archivo se ejecuta de manera correcta
+        }).done(function (respuesta) { 
+            //simulando tiempo de retardo en respuesta de un servidor
+            //cuando cambie el estado a cargado, se mostrará la información proveniente del servidor 
+            setTimeout(function () {
+                $("#resultado2").html(respuesta);
+            },3000);
+            // el metodo always, se ejecuta siempre, exista error o no en el llamado y la respuesta.
         }).always(function () {
             setTimeout(mostrar,3000);
         });
