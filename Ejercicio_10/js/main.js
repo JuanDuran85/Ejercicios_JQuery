c34ef5981bc395cfb15796796f7a52c0b309b635
JuanDuran85 <duran85juan@gmail.com>
2020-02-22 23:06:26 -0300

    agregando ejercicio 10

--- Ejercicio_10/js/main.js
+++ Ejercicio_10/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
-    $("#main__contenedor");
+    $("#main__contenedor").on("click", "a", function (event) {
+        //para desactivar los comportamientos predeterminados de formularios, link, se utiliza el metodo de preventDefault 
+        event.preventDefault();
+        //Para detener la propagacion del evento y que no se ejecute en otros elementos del DOM, se utiliza el metodo stopPropagation
+        event.stopPropagation();
+        console.log("clic solo en el texto");
+        alert("clic en texto con la etiqueta a");
+        $("#div__resultado").html("<p>clic solo en el texto</p>");
+    });
+
+    $("#main__contenedor").on("click", function () {  
+        console.log("clic sobre el contendor");
+        $("#div__resultado").html("<p>clic en el contenedor principal</p>");
+    })
 });
