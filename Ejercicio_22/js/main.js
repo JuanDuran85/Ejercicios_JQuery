be81ddd4d08b7be3be5b25fc1bfe5af6d8c7d4da
JuanDuran85 <duran85juan@gmail.com>
2020-02-25 00:02:59 -0300

    arreglando ejercicio 22

--- Ejercicio_22/js/main.js
+++ Ejercicio_22/js/main.js
@@ -17,29 +17,55 @@ $(function () {
             };
             //console.log("elemento 0");
             //console.log(elementos[0].zip); 
+            var mostrar = `
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Id</td>
+                        <td>ZIP</td>
+                        <td>Country</td>
+                    </tr>
+            `;
+
             elementos.forEach((value, index, array) => {
-                console.log("elementos");
+/*                 console.log("elementos");
                 console.log(value);
                 console.log(index);
-                console.log(array);
+                console.log(array); */
+                if (index <= 20) {
+                    mostrar += `
+                        <tr>
+                            <td>${index}</td>
+                            <td>${value.zip}</td>
+                            <td>${value.country}</td>
+                        </tr>
+                        `;
+                }
             });
-            for (let index = 0; index < 20; index++) {
-                var mostrar = `
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>ZIP</td>
-                                    <td>Country</td>
-                                </tr>
-                                <tr>
-                                    <td>${elementos[index].zip}</td>
-                                    <td>${elementos[index].country}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    `;
-                    $("body").append(mostrar);    
-            }; 
+
+            mostrar += `
+                    </tbody>
+                    </table>
+            `;
+            $(".text").text("Cargado");
+            $("body").append(mostrar); 
+            // for (let index = 0; index < 20; index++) {
+            //     var mostrar = `
+            //             <table>
+            //                 <tbody>
+            //                     <tr>
+            //                         <td>ZIP</td>
+            //                         <td>Country</td>
+            //                     </tr>
+            //                     <tr>
+            //                         <td>${elementos[index].zip}</td>
+            //                         <td>${elementos[index].country}</td>
+            //                     </tr>
+            //                 </tbody>
+            //             </table>
+            //         `;
+            //         $("body").append(mostrar);    
+            // }; 
         },
         dataType: "jsonp"
       });
