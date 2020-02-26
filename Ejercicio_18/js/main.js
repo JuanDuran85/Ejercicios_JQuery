2dca445341a8d1d84d8ef4f03a121d92bddf4adf
JuanDuran85 <duran85juan@gmail.com>
2020-02-26 12:06:10 -0300

    agregando cambios

--- Ejercicio_18/js/main.js
+++ Ejercicio_18/js/main.js
@@ -9,12 +9,12 @@ $(function () {
         $.each(respuesta, function (indexInArray, valueOfElement) { 
             console.log("Index: "+indexInArray);
             console.log(valueOfElement);
-            mostrar = '<section id="contenedor__caja" class="contenedor__caja" data-id="'+valueOfElement.id+'">';
+            mostrar = '<section id="contenedor__caja" class="contenedor__caja col-4 m-1" data-id="'+valueOfElement.id+'">';
             mostrar += '<img src="'+valueOfElement.imagen+'" >';
             mostrar += '<div class="titulo__caja">'+valueOfElement.nombre+'</div>';
             mostrar += '<p>'+valueOfElement.descripcion+'</p>';
             mostrar += '<div class="precio__caja">'+valueOfElement.precio+'CLP</div>';
-            mostrar += '<button>Agregar a Carro</button>';
+            mostrar += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Agregar a Carro</button>';
             mostrar += `
                 <div>
                     <a href="#" class="informa__caja">Más Información Aquí</a>
@@ -23,7 +23,28 @@ $(function () {
                     <p>${valueOfElement.masInfo}</p>
                 </div>
             `;
-            $("body").append(mostrar);
+            mostrar += `
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Compra agregada al carro. Precio seleccionado ${valueOfElement.precio}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+                </div>
+            </div>
+            </div>   
+            `;
+            $(".muestra").append(mostrar);
         });
     });
 
--- Ejercicio_24/js/main.js
+++ Ejercicio_24/js/main.js
@@ -3,10 +3,18 @@ $(function () {
         evento.preventDefault();
         var valor_numero = $("#numePo").val();
         console.log(valor_numero);
+
           $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon/${valor_numero}/`,
-          }).done(function( data ) {
-              console.log( "Sample of data:", data );
+              url: `https://pokeapi.co/api/v2/pokemon/${valor_numero}/`,
+              success: function (data) {
+                //console.log(data);
+                //console.log(data.abilities);
+                $.each(data.abilities, function (indexInArray, valueOfElement) { 
+                    console.log(indexInArray); 
+                    console.log(valueOfElement.ability.url);
+                  $(".pokeInfo").append(`<p>${valueOfElement.ability.url}</p>`);
+                });
+              }
             });
       });
 });
